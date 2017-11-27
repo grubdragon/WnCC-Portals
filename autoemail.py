b1c181240d4ec80d3499981144e80a71e0ff441b
TheFlash98 <mastermind.sarthak@gmail.com>
2017-11-28 00:37:32 +0530

    Complete mailing

--- autoemail.py
+++ autoemail.py
@@ -4,7 +4,5 @@ subprocess.call("sqlite3 database.db \"SELECT * FROM email\" > result.txt",shell
 
 f = open("result.txt","r")
 emails = f.readlines()
-print emails
 for email in emails:
-	com = "echo \"This is a test.\" | mail -s \"test message\" "+email
-	subprocess.call(com,shell=True)
+	subprocess.call(['bash','mailer.sh',email])
