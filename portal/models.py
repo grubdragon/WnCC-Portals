db1e750e6007df862d3ede5d46f684004bbf56d9
ydidwania <didwaniayashvardhan+github@gmail.com>
2017-12-03 05:32:13 +0530

    Edit Form working

--- portal/models.py
+++ portal/models.py
@@ -19,7 +19,7 @@ class Tag(models.Model):
 
 class Student(models.Model):
 
-    YEAR_CHOICES=[(c,c) for c in [1,2,3,4,5]]
+    #YEAR_CHOICES=[(c,c) for c in [1,2,3,4,5]]
     PROGRAM_CHOICES=[(c,c) for c in['Bachelors','Masters','Doctorate','Post-Doctorate']]
     LOGIN_CHOICES = [
             ('S','SSO'),
@@ -29,10 +29,10 @@ class Student(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     mode_of_login = models.CharField(max_length=1,choices=LOGIN_CHOICES,default='S')
-    college_name = models.CharField(max_length=200)
+    college = models.CharField(max_length=200)
     contact_no = models.CharField(max_length=10)
     email = models.EmailField()
-    year = models.IntegerField(choices=YEAR_CHOICES,default=1)
+    year = models.IntegerField(default=1)
     programme_of_study = models.CharField(max_length=20,choices=PROGRAM_CHOICES,null=True)
     resume = models.URLField(blank=True)
     github = models.URLField(blank=True)
--- portal/views.py
+++ portal/views.py
@@ -5,26 +5,32 @@ from models import Student,Tag
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render,reverse,redirect
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 
+@csrf_protect
 @login_required
 def profile(request):
     name = request.user.first_name +' ' + request.user.last_name
     email = request.user.email
-    try:
-        student=Student.objects.get(name=name,email=email,mode_of_login='G')
-    except Student.DoesNotExist:
-        student = Student.objects.create(name=name,email=email,mode_of_login='G')
+    
 
     if request.method == 'POST':
-        form = StudentForm(request.POST, instance=student) 
+        student = get_object_or_404(Student,name=name,email=email,mode_of_login='G')
+        form = StudentForm(request.POST, instance=student)
+        print request.POST
+        #return render(request , 'reg.html',{'form':form})
         if form.is_valid():
             form.save()
             return redirect(reverse('login'))
 
     
     else:
+        try:
+            student=Student.objects.get(name=name,email=email,mode_of_login='G')
+        except Student.DoesNotExist:
+            student = Student.objects.create(name=name,email=email,mode_of_login='G')
         form = StudentForm(instance=student)
         return render(request , 'profile.html',{'student':student})
 
